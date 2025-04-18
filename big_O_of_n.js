2fac462d6e1b84826f10311b0a70196d346c2e0b
Akash <akash.v.harale@gmail.com>
2025-04-18 20:32:03 +0530

    eliminate constants in the O(n)

--- big_O_of_n.js
+++ big_O_of_n.js
@@ -4,11 +4,10 @@ const logItems = (n) => {
   }
 };
 
-logItems(9)
+logItems(9);
 
-
-
-{ /*
+{
+  /*
   there are theree types of time complexity
  - omega -ideal time complexity 
  - theta - average time complexity
@@ -20,5 +19,28 @@ logItems(9)
 - becuase if you want to find a number in the array and that number is at the 
 last position then you have to iterate thorough the whole array and at the end
 you will find out that number , that why we consider only bog O
- */}
+ */
+}
 
+const eliminateConstants = (n) => {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  for (let j = 0; j < n; j++) {
+    console.log(i);
+  }
+};
+
+console.log(eliminateConstants(10));
+
+{
+  /* 
+  
+Here the first loop time complexity => O(n)
+and second loop time complexity => O(n)
+
+by adding it becomes => O(2n)
+but we should eliminate constants so that becomes => O(n)
+  
+  */
+}
